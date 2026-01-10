976ae5d31239ab194b6dfb9db9d10dcd9cbcfe86
Alexander <alkuzindev@gmail.com>
2026-01-10 16:12:34 +0300

    [feat]: added application configurations module

---
+++ ahrs-monitor/src/config.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0.
+// Copyright (C) 2026-present ahrs-monitor project and contributors.
+
+//! Application's configurations.
+
+/// Window width in pixels.
+pub const APP_WINDOW_WIDTH: f32 = 1024.0;
+
+/// Window height in pixels.
+pub const APP_WINDOW_HEIGHT: f32 = 768.0;
+
+/// Window size in pixels.
+pub const APP_WINDOW_SIZE: [f32; 2] = [APP_WINDOW_WIDTH, APP_WINDOW_HEIGHT];
+
+/// Title of the window.
+pub const APP_WINDOW_TITLE: &'static str = "AHRS Monitor";
