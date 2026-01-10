79629e4527678b9dc082cc83bbb90c4a148334cf
Alexander <alkuzindev@gmail.com>
2026-01-10 14:59:39 +0300

    [info]: added ahrs-monitor crate

---
+++ ahrs-monitor/src/main.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0.
+// Copyright (C) 2026-present ahrs-monitor project and contributors.
+
+//! AHRS monitor entry point.
+
+fn main() {
+    println!("Running AHRS monitor...");
+}
