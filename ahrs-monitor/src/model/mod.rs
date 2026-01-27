837562f0669439826291ef44af3f56778153f71a
Alexander <alkuzindev@gmail.com>
2026-01-27 14:33:40 +0300

    [feat]: added FrameContext struct

--- ahrs-monitor/src/model/mod.rs
+++ ahrs-monitor/src/model/mod.rs
@@ -3,9 +3,23 @@
 
 //! Application state module.
 
+use tsilna_nav::protocol::idtp::IdtpFrame;
+
+#[derive(Debug, Default)]
+pub struct FrameContext {
+    /// IMU Data Transfer Protocol frame.
+    pub frame: Option<IdtpFrame>,
+    /// Total number of packets.
+    pub total_packets: usize,
+    /// Number of broken packets.
+    pub bad_packets: usize,
+}
+
 /// Application events enumeration.
 #[derive(Debug)]
 pub enum AppEvent {
     /// Event for updating IMU connection status.
     UpdateConnectionStatus(bool),
+    /// Event for handling received frame.
+    FrameReceived(FrameContext),
 }
