fcfa8474e6622475043684d32f8448f4de2c8bdb
Alexander <alkuzindev@gmail.com>
2026-01-28 13:21:35 +0300

    [feat]: updated IMU simulator

--- ahrs-monitor/examples/imu_simulator.rs
+++ ahrs-monitor/examples/imu_simulator.rs
@@ -3,170 +3,11 @@
 
 //! Simulator of sending IDTP frames over UDP.
 
+use tsilna_nav::{protocol::idtp::{IdtpHeader, IdtpFrame, Mode}, math::rng::Xorshift};
+use ahrs_monitor::model::payload::Payload;
+use zerocopy::IntoBytes;
 use tokio::{net::UdpSocket, time::Instant};
-use tsilna_nav::protocol::idtp;
-use std::{time::Duration, mem};
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("127.0.0.1:10001").await?;
-    let target_addr = "127.0.0.1:10000";
-
-    let mut sequence = 0u32;
-    let start_time = Instant::now();
-
-    loop {
-        let timestamp = start_time.elapsed().as_millis();
-        let payload = generate_payload(0);
-
-        let mut header = IdtpHeader::new();
-        header.mode = Mode::Safety as u8;
-        header.device_id = 0xABCD;
-        header.sequence = sequence;
-        header.timestamp = start_time.elapsed().as_micros() as u32;
-
-        let mut frame = IdtpFrame::new();
-        frame.set_header(&header);
-        let _ = frame.set_payload(&payload.as_bytes());
-
-        let mut buffer = [0u8; 196];
-
-        let _ = frame.pack(&mut buffer, None);
-        socket.send_to(&buffer, target_addr).await?;
-
-        sequence += 1;
-
-        if sequence % 100 == 0 {
-            println!("Sent 100 packets, current timestamp: {} ms", timestamp);
-        }
-
-        tokio::time::sleep(Duration::from_millis(5)).await;
-    }
-}
-
-/// IDTP payload struct.
-#[derive(Debug, Default, Clone, Copy)]
-#[repr(C, packed)]
-pub struct Payload {
-    pub acc1_x: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 1)
-    /// along the Y axis (m/s^2).
-    pub acc1_y: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 1)
-    /// along the Z axis (m/s^2).
-    pub acc1_z: f32,
-    /// Angular velocity (gyroscope 1) along the X axis (rad/s).
-    pub gyr1_x: f32,
-    /// Angular velocity (gyroscope 1) along the Y axis (rad/s).
-    pub gyr1_y: f32,
-    /// Angular velocity (gyroscope 1) along the Z axis (rad/s).
-    pub gyr1_z: f32,
-    /// (Magnetometer 1) value along the X axis (Gauss).
-    pub mag1_x: f32,
-    /// (Magnetometer 1) value along the Y axis (Gauss).
-    pub mag1_y: f32,
-    /// (Magnetometer 1) value along the Z axis (Gauss).
-    pub mag1_z: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 2)
-    /// along the X axis (m/s^2).
-    pub acc2_x: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 2)
-    /// along the Y axis (m/s^2).
-    pub acc2_y: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 2)
-    /// along the Z axis (m/s^2).
-    pub acc2_z: f32,
-    /// Angular velocity (gyroscope 2) along the X axis (rad/s).
-    pub gyr2_x: f32,
-    /// Angular velocity (gyroscope 2) along the Y axis (rad/s).
-    pub gyr2_y: f32,
-    /// Angular velocity (gyroscope 2) along the Z axis (rad/s).
-    pub gyr2_z: f32,
-    /// (Magnetometer 2) value along the X axis (Gauss).
-    pub mag2_x: f32,
-    /// (Magnetometer 2) value along the Y axis (Gauss).
-    pub mag2_y: f32,
-    /// (Magnetometer 2) value along the Z axis (Gauss).
-    pub mag2_z: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 3)
-    /// along the X axis (m/s^2).
-    pub acc3_x: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 3)
-    /// along the Y axis (m/s^2).
-    pub acc3_y: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 3)
-    /// along the Z axis (m/s^2).
-    pub acc3_z: f32,
-    /// Angular velocity (gyroscope 3) along the X axis (rad/s).
-    pub gyr3_x: f32,
-    /// Angular velocity (gyroscope 3) along the Y axis (rad/s).
-    pub gyr3_y: f32,
-    /// Angular velocity (gyroscope 3) along the Z axis (rad/s).
-    pub gyr3_z: f32,
-    /// (Magnetometer 3) value along the X axis (Gauss).
-    pub mag3_x: f32,
-    /// (Magnetometer 3) value along the Y axis (Gauss).
-    pub mag3_y: f32,
-    /// (Magnetometer 3) value along the Z axis (Gauss).
-    pub mag3_z: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 4)
-    /// along the X axis (m/s^2).
-    pub acc4_x: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 4)
-    /// along the Y axis (m/s^2).
-    pub acc4_y: f32,
-    /// The value of the projection of the acceleration vector (accelerometer 4)
-    /// along the Z axis (m/s^2).
-    pub acc4_z: f32,
-    /// Angular velocity (gyroscope 4) along the X axis (rad/s).
-    pub gyr4_x: f32,
-    /// Angular velocity (gyroscope 4) along the Y axis (rad/s).
-    pub gyr4_y: f32,
-    /// Angular velocity (gyroscope 4) along the Z axis (rad/s).
-    pub gyr4_z: f32,
-    /// (Magnetometer 4) value along the X axis (Gauss).
-    pub mag4_x: f32,
-    /// (Magnetometer 4) value along the Y axis (Gauss).
-    pub mag4_y: f32,
-    /// (Magnetometer 4) value along the Z axis (Gauss).
-    pub mag4_z: f32,
-    /// Pressure value (barometer 1) (Pascal).
-    pub baro1: f32,
-    /// Pressure value (barometer 2) (Pascal).
-    pub baro2: f32,
-    /// Pressure value (barometer 3) (Pascal).
-    pub baro3: f32,
-    /// Pressure value (barometer 4) (Pascal).
-    pub baro4: f32,
-}
-
-/// Payload size in bytes.
-pub const PAYLOAD_SIZE: usize = size_of::<Payload>();
-
-impl Payload {
-    /// Convert payload to bytes.
-    ///
-    /// # Returns
-    /// - Payload byte array.
-    pub fn as_bytes(&self) -> [u8; PAYLOAD_SIZE] {
-        unsafe { mem::transmute::<Self, [u8; PAYLOAD_SIZE]>(*self) }
-    }
-
-    /// Convert a byte slice to a `Payload` struct.
-    ///
-    /// # Parameters
-    /// - `bytes` - given bytes to convert.
-    ///
-    /// # Returns
-    /// - Payload from bytes.
-    pub fn from_bytes(bytes: &[u8; PAYLOAD_SIZE]) -> Self {
-        unsafe { mem::transmute::<[u8; PAYLOAD_SIZE], Self>(*bytes) }
-    }
-}
-
-use core::ops::Range;
-use idtp::{IdtpFrame, IdtpHeader, Mode};
-use rand::Rng;
+use std::{ops::Range, time::Duration};
 
 /// Pseudo-random accelerometer readings range.
 const RNG_ACC_RANGE: Range<f32> = -39.22..39.22; // +-4g.
@@ -177,9 +18,6 @@ const RNG_GYR_RANGE: Range<f32> = -34.91..34.91; // 2000 DPS.
 /// Pseudo-random magnetometer readings range.
 const RNG_MAG_RANGE: Range<f32> = -8.0..8.0; // +-8 Gauss.
 
-/// Pseudo-random barometer readings range.
-const RNG_BARO_RANGE: Range<f32> = 95000.0..105000.0; // ~1 atm.
-
 /// Generate payload with pseudo-random IMU sensors readings.
 ///
 /// # Parameters
@@ -188,53 +26,58 @@ const RNG_BARO_RANGE: Range<f32> = 95000.0..105000.0; // ~1 atm.
 /// # Returns
 /// - New generated payload.
 pub fn generate_payload(state: u32) -> Payload {
-    let mut rng = rand::rng();
+    let mut rng = Xorshift::new(state);
     let mut payload = Payload::default();
 
-    payload.acc1_x = rng.random_range(RNG_ACC_RANGE);
-    payload.acc1_y = rng.random_range(RNG_ACC_RANGE);
-    payload.acc1_z = rng.random_range(RNG_ACC_RANGE);
-    payload.gyr1_x = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr1_y = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr1_z = rng.random_range(RNG_GYR_RANGE);
-    payload.mag1_x = rng.random_range(RNG_MAG_RANGE);
-    payload.mag1_y = rng.random_range(RNG_MAG_RANGE);
-    payload.mag1_z = rng.random_range(RNG_MAG_RANGE);
-
-    payload.acc2_x = rng.random_range(RNG_ACC_RANGE);
-    payload.acc2_y = rng.random_range(RNG_ACC_RANGE);
-    payload.acc2_z = rng.random_range(RNG_ACC_RANGE);
-    payload.gyr2_x = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr2_y = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr2_z = rng.random_range(RNG_GYR_RANGE);
-    payload.mag2_x = rng.random_range(RNG_MAG_RANGE);
-    payload.mag2_y = rng.random_range(RNG_MAG_RANGE);
-    payload.mag2_z = rng.random_range(RNG_MAG_RANGE);
-
-    payload.acc3_x = rng.random_range(RNG_ACC_RANGE);
-    payload.acc3_y = rng.random_range(RNG_ACC_RANGE);
-    payload.acc3_z = rng.random_range(RNG_ACC_RANGE);
-    payload.gyr3_x = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr3_y = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr3_z = rng.random_range(RNG_GYR_RANGE);
-    payload.mag3_x = rng.random_range(RNG_MAG_RANGE);
-    payload.mag3_y = rng.random_range(RNG_MAG_RANGE);
-    payload.mag3_z = rng.random_range(RNG_MAG_RANGE);
-
-    payload.acc4_x = rng.random_range(RNG_ACC_RANGE);
-    payload.acc4_y = rng.random_range(RNG_ACC_RANGE);
-    payload.acc4_z = rng.random_range(RNG_ACC_RANGE);
-    payload.gyr4_x = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr4_y = rng.random_range(RNG_GYR_RANGE);
-    payload.gyr4_z = rng.random_range(RNG_GYR_RANGE);
-    payload.mag4_x = rng.random_range(RNG_MAG_RANGE);
-    payload.mag4_y = rng.random_range(RNG_MAG_RANGE);
-    payload.mag4_z = rng.random_range(RNG_MAG_RANGE);
-
-    payload.baro1 = rng.random_range(RNG_BARO_RANGE);
-    payload.baro2 = rng.random_range(RNG_BARO_RANGE);
-    payload.baro3 = rng.random_range(RNG_BARO_RANGE);
-    payload.baro4 = rng.random_range(RNG_BARO_RANGE);
-
+    payload.acc_x = rng.next_f32(RNG_ACC_RANGE);
+    payload.acc_y = rng.next_f32(RNG_ACC_RANGE);
+    payload.acc_z = rng.next_f32(RNG_ACC_RANGE);
+    payload.gyr_x = rng.next_f32(RNG_GYR_RANGE);
+    payload.gyr_y = rng.next_f32(RNG_GYR_RANGE);
+    payload.gyr_z = rng.next_f32(RNG_GYR_RANGE);
+    payload.mag_x = rng.next_f32(RNG_MAG_RANGE);
+    payload.mag_y = rng.next_f32(RNG_MAG_RANGE);
+    payload.mag_z = rng.next_f32(RNG_MAG_RANGE);
     payload
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:10001").await?;
+    let target_addr = "127.0.0.1:10000";
+
+    let mut sequence = 0u32;
+    let mut buffer = vec![0u8; 64];
+    let mut rng_state = 1;
+    let delay_time = Duration::from_millis(5); // 5 ms (200 Hz).
+    let start_time = Instant::now();
+
+    loop {
+        let payload = generate_payload(rng_state);
+
+        // Setting the IDTP header.
+        let mut header = IdtpHeader::new();
+        header.mode = Mode::Safety as u8;
+        header.device_id = 0xABCD;
+        header.sequence = sequence;
+        header.timestamp = start_time.elapsed().as_micros() as u32;
+
+        // Setting the IDTP frame.
+        let mut frame = IdtpFrame::new();
+        frame.set_header(&header);
+        let _ = frame.set_payload(&payload.as_bytes());
+
+        let frame_size = frame.size().unwrap_or(0);
+        let _ = frame.pack(&mut buffer[..frame_size], None);
+        socket.send_to(&buffer, target_addr).await?;
+
+        sequence += 1;
+
+        if sequence % 100 == 0 {
+            println!("Sequence: {} Sent 100 packets", sequence);
+        }
+
+        tokio::time::sleep(delay_time).await;
+        rng_state += 1;
+    }
 }
