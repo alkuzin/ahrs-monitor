aa9a0ea98298321b6a9f089f05a31c8c08833a04
Alexander <alkuzindev@gmail.com>
2026-02-05 10:12:44 +0300

    [refactor]: moved IdtpStandardPayload enum declarations to core module

--- ahrs-monitor/src/bin/imu_simulator/utils.rs
+++ ahrs-monitor/src/bin/imu_simulator/utils.rs
@@ -3,63 +3,9 @@
 
 //! Utils for IMU simulator.
 
-use ahrs_monitor::config::ImuMetrics;
-use std::ops::Range;
+use ahrs_monitor::{config::ImuMetrics, core::IdtpStandardPayload};
 use tsilna_nav::{math::rng::Xorshift, protocol::idtp::payload::*};
-
-/// IDTP standard payload enumeration.
-pub enum IdtpStandardPayload {
-    /// Accelerometer only (for 3-axis sensor).
-    Imu3Acc(Imu3Acc),
-    /// Gyroscope only (for 3-axis sensor).
-    Imu3Gyr(Imu3Gyr),
-    /// Magnetometer only (for 3-axis sensor).
-    Imu3Mag(Imu3Mag),
-    /// Accelerometer + Gyroscope readings (for 6-axis sensor).
-    Imu6(Imu6),
-    /// Accelerometer + Gyroscope + Magnetometer readings
-    /// (for 9-axis sensor).
-    Imu9(Imu9),
-    /// Accelerometer + Gyroscope + Magnetometer + Barometer readings
-    /// (for 10-axis sensor).
-    Imu10(Imu10),
-    /// Attitude. Hamiltonian Quaternion (w, x, y, z).
-    ImuQuat(ImuQuat),
-}
-
-impl IdtpStandardPayload {
-    /// Convert payload to bytes.
-    ///
-    /// # Returns
-    /// - Bytes representation of payload.
-    pub fn to_bytes(&self) -> &[u8] {
-        match self {
-            IdtpStandardPayload::Imu3Acc(p) => p.to_bytes(),
-            IdtpStandardPayload::Imu3Gyr(p) => p.to_bytes(),
-            IdtpStandardPayload::Imu3Mag(p) => p.to_bytes(),
-            IdtpStandardPayload::Imu6(p) => p.to_bytes(),
-            IdtpStandardPayload::Imu9(p) => p.to_bytes(),
-            IdtpStandardPayload::Imu10(p) => p.to_bytes(),
-            IdtpStandardPayload::ImuQuat(p) => p.to_bytes(),
-        }
-    }
-
-    /// Get payload type.
-    ///
-    /// # Returns
-    /// - Payload type according to IDTP specification.
-    pub fn payload_type(&self) -> u8 {
-        match self {
-            IdtpStandardPayload::Imu3Acc(_) => Imu3Acc::TYPE_ID,
-            IdtpStandardPayload::Imu3Gyr(_) => Imu3Gyr::TYPE_ID,
-            IdtpStandardPayload::Imu3Mag(_) => Imu3Mag::TYPE_ID,
-            IdtpStandardPayload::Imu6(_) => Imu6::TYPE_ID,
-            IdtpStandardPayload::Imu9(_) => Imu9::TYPE_ID,
-            IdtpStandardPayload::Imu10(_) => Imu10::TYPE_ID,
-            IdtpStandardPayload::ImuQuat(_) => ImuQuat::TYPE_ID,
-        }
-    }
-}
+use std::ops::Range;
 
 /// Pseudo-random accelerometer readings range.
 const RNG_ACC_RANGE: Range<f32> = -157.0..157.0; // +- 16g (m/s^2)
