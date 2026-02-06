286d50b4a7b3720d2c71f3475169f274273028cb
Alexander <alkuzindev@gmail.com>
2026-02-06 15:34:41 +0300

    [feat]: added Logger struct & rewrited LogRecord struct

--- ahrs-monitor/src/logger.rs
+++ ahrs-monitor/src/logger.rs
@@ -4,25 +4,50 @@
 //! IMU data logger implementation.
 
 use tsilna_nav::protocol::idtp::payload::{Imu10, Imu3Acc, Imu3Gyr, Imu3Mag, Imu6, Imu9, ImuQuat};
+use chrono::Local;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Default, Serialize, Deserialize)]
 /// IMU data log record.
 pub struct LogRecord {
     /// Timestamp represents the sensor-local time.
     pub timestamp: u32,
     /// Vendor-specific unique IMU device identifier.
     pub device_id: u16,
-    /// Accelerometer readings.
-    pub acc: Option<[f32; 3]>,
-    /// Gyroscope readings.
-    pub gyr: Option<[f32; 3]>,
-    /// Magnetometer readings.
-    pub mag: Option<[f32; 3]>,
-    /// Barometer readings.
-    pub baro: Option<f32>,
-    /// Attitude (Quaternion).
-    pub quat: [f32; 4],
-    /// Attitude (Euler angles).
-    pub euler: [f32; 3],
+    /// Accelerometer reading along X-axis.
+    pub acc_x: Option<f32>,
+    /// Accelerometer reading along for Y-axis.
+    pub acc_y: Option<f32>,
+    /// Accelerometer reading along for Z-axis.
+    pub acc_z: Option<f32>,
+    /// Gyroscope reading along the X-axis.
+    pub gyr_x: Option<f32>,
+    /// Gyroscope reading along the Y-axis.
+    pub gyr_y: Option<f32>,
+    /// Gyroscope reading along the Z-axis.
+    pub gyr_z: Option<f32>,
+    /// Magnetometer reading along the X-axis.
+    pub mag_x: Option<f32>,
+    /// Magnetometer reading along the Y-axis.
+    pub mag_y: Option<f32>,
+    /// Magnetometer reading along the Z-axis.
+    pub mag_z: Option<f32>,
+    /// Barometer reading.
+    pub pressure: Option<f32>,
+    /// Quaternion scalar component W.
+    pub q_w: f32,
+    /// Quaternion vector component X.
+    pub q_x: f32,
+    /// Quaternion vector component Y.
+    pub q_y: f32,
+    /// Quaternion vector component Z.
+    pub q_z: f32,
+    /// Rotation around X-axis.
+    pub roll: f32,
+    /// Rotation around Y-axis.
+    pub pitch: f32,
+    /// Rotation around Z-axis.
+    pub yaw: f32,
 }
 
 /// Trait for logging IDTP frame payload data.
@@ -36,62 +61,145 @@ pub trait ToLog {
 
 impl ToLog for Imu3Acc {
     fn fill_record(&self, record: &mut LogRecord) {
-        let acc_data = [self.acc_x, self.acc_y, self.acc_z];
-        record.acc = Some(acc_data);
+        record.acc_x = Some(self.acc_x);
+        record.acc_y = Some(self.acc_y);
+        record.acc_z = Some(self.acc_z);
     }
 }
 
 impl ToLog for Imu3Gyr {
     fn fill_record(&self, record: &mut LogRecord) {
-        let gyr_data = [self.gyr_x, self.gyr_y, self.gyr_z];
-        record.gyr = Some(gyr_data);
+        record.gyr_x = Some(self.gyr_x);
+        record.gyr_y = Some(self.gyr_y);
+        record.gyr_z = Some(self.gyr_z);
     }
 }
 
 impl ToLog for Imu3Mag {
     fn fill_record(&self, record: &mut LogRecord) {
-        let mag_data = [self.mag_x, self.mag_y, self.mag_z];
-        record.mag = Some(mag_data);
+        record.mag_x = Some(self.mag_x);
+        record.mag_y = Some(self.mag_y);
+        record.mag_z = Some(self.mag_z);
     }
 }
 
 impl ToLog for Imu6 {
     fn fill_record(&self, record: &mut LogRecord) {
-        let acc_data = [self.acc.acc_x, self.acc.acc_y, self.acc.acc_z];
-        let gyr_data = [self.gyr.gyr_x, self.gyr.gyr_y, self.gyr.gyr_z];
-
-        record.acc = Some(acc_data);
-        record.gyr = Some(gyr_data);
+        record.acc_x = Some(self.acc.acc_x);
+        record.acc_y = Some(self.acc.acc_y);
+        record.acc_z = Some(self.acc.acc_z);
+        record.gyr_x = Some(self.gyr.gyr_x);
+        record.gyr_y = Some(self.gyr.gyr_y);
+        record.gyr_z = Some(self.gyr.gyr_z);
     }
 }
 
 impl ToLog for Imu9 {
     fn fill_record(&self, record: &mut LogRecord) {
-        let acc_data = [self.acc.acc_x, self.acc.acc_y, self.acc.acc_z];
-        let gyr_data = [self.gyr.gyr_x, self.gyr.gyr_y, self.gyr.gyr_z];
-        let mag_data = [self.mag.mag_x, self.mag.mag_y, self.mag.mag_z];
-
-        record.acc = Some(acc_data);
-        record.gyr = Some(gyr_data);
-        record.mag = Some(mag_data);
+        record.acc_x = Some(self.acc.acc_x);
+        record.acc_y = Some(self.acc.acc_y);
+        record.acc_z = Some(self.acc.acc_z);
+        record.gyr_x = Some(self.gyr.gyr_x);
+        record.gyr_y = Some(self.gyr.gyr_y);
+        record.gyr_z = Some(self.gyr.gyr_z);
+        record.mag_x = Some(self.mag.mag_x);
+        record.mag_y = Some(self.mag.mag_y);
+        record.mag_z = Some(self.mag.mag_z);
     }
 }
 
 impl ToLog for Imu10 {
     fn fill_record(&self, record: &mut LogRecord) {
-        let acc_data = [self.acc.acc_x, self.acc.acc_y, self.acc.acc_z];
-        let gyr_data = [self.gyr.gyr_x, self.gyr.gyr_y, self.gyr.gyr_z];
-        let mag_data = [self.mag.mag_x, self.mag.mag_y, self.mag.mag_z];
-
-        record.acc = Some(acc_data);
-        record.gyr = Some(gyr_data);
-        record.mag = Some(mag_data);
-        record.baro = Some(self.baro);
+        record.acc_x = Some(self.acc.acc_x);
+        record.acc_y = Some(self.acc.acc_y);
+        record.acc_z = Some(self.acc.acc_z);
+        record.gyr_x = Some(self.gyr.gyr_x);
+        record.gyr_y = Some(self.gyr.gyr_y);
+        record.gyr_z = Some(self.gyr.gyr_z);
+        record.mag_x = Some(self.mag.mag_x);
+        record.mag_y = Some(self.mag.mag_y);
+        record.mag_z = Some(self.mag.mag_z);
+        record.pressure = Some(self.baro);
     }
 }
 
 impl ToLog for ImuQuat {
     fn fill_record(&self, record: &mut LogRecord) {
-        record.quat = [self.w, self.x, self.y, self.z];
+        record.q_w = self.w;
+        record.q_x = self.x;
+        record.q_y = self.y;
+        record.q_z = self.z;
+    }
+}
+
+/// IMU data log records handler.
+pub struct Logger {
+    /// CSV file writer.
+    writer: csv::Writer<std::fs::File>,
+    /// Path to log file.
+    path: String,
+    /// Recording start timestamp.
+    start_time: std::time::Instant,
+}
+
+impl Logger {
+    /// Construct new `Logger` object.
+    ///
+    /// # Returns
+    /// - New `Logger` object - in case of success.
+    /// - `Err` - otherwise.
+    ///
+    /// # Errors
+    /// - I/O errors.
+    /// - Error to create log file.
+    pub fn new() -> std::io::Result<Self> {
+        // TODO: set specific logging directory.
+        let now = Local::now();
+        let path = format!("log_{}.csv", now.format("%d-%m-%Y_%H-%M-%S"));
+        let file = std::fs::File::create(&path)?;
+
+        Ok(Self {
+            writer: csv::Writer::from_writer(file),
+            path,
+            start_time: std::time::Instant::now(),
+        })
+    }
+
+    /// Get log file path.
+    pub fn path(&self) -> &String {
+        &self.path
+    }
+
+    /// Write record into the log file.
+    ///
+    /// # Parameters
+    /// - `record` - given IMU data log record to handle.
+    ///
+    /// # Returns
+    /// - `Ok` - in case of success.
+    /// - `Err` - otherwise.
+    ///
+    /// # Errors
+    /// - I/O errors.
+    /// - CSV file handling errors.
+    pub fn write(&mut self, record: &LogRecord) -> anyhow::Result<()> {
+        self.writer.serialize(record)?;
+        self.writer.flush()?;
+
+        Ok(())
+    }
+
+    /// Get timestamp since the start of the recording.
+    ///
+    /// # Returns
+    /// - Timestamp in string representation.
+    pub fn timestamp_str(&self) -> String {
+        let elapsed = self.start_time.elapsed();
+        let secs = elapsed.as_secs();
+        let seconds = secs % 60;
+        let minutes = (secs / 60) % 60;
+        let hours = secs / 3600;
+
+        format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
     }
 }
